d2a29b510750ca8f7e23e51d80febc194a366701
upsugar <95656392+upsugar@users.noreply.github.com>
2022-03-31 20:27:43 +0300

    Add files via upload

---
+++ precache-manifest.4edb7e3a9519dbc409334a536c09622d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2e95f29cccfa14174dcf6364f640e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ae3fd35bd1708479f03",
+    "url": "/js/app.ae377c66.js"
+  },
+  {
+    "revision": "a3873639a6223bac2cca",
+    "url": "/js/chunk-vendors.b53b4d9f.js"
+  },
+  {
+    "revision": "03b8ad800b22244878af354b0662e153",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
